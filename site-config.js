a51379179750f2088ab436936e1677826d9d99b8
JackieLee250209 <JackieLee250209@users.noreply.github.com>
2026-08-31 14:33:15 +0800

    Use Creem checkout for product license

--- site-config.js
+++ site-config.js
@@ -1,6 +1,6 @@
 window.DINORUN_SITE_CONFIG = {
-  // PayPal payment link for the US$70 Dinorun license.
-  checkoutUrl: "https://www.paypal.com/ncp/payment/QBFSNNFBZ8YJJ",
+  // Creem checkout link for the US$70 Dinorun license.
+  checkoutUrl: "https://www.creem.io/payment/prod_2ZXC9d6AuM8RCzxtco9Riq",
 
   // The primary public address must match the address in Creem Business Details.
   supportEmails: ["leehonxuan@gmail.com", "38784757@qq.com"]
